484c32d7721817bcbe8d76a1b702cdc8de04ff78
Vi Dev <vi@videv.fr>
2024-12-02 20:39:33 +0100

    📦 TSE-01: Convert App.jsx to App.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client"
-import App from "./App.jsx"
+import App from "./App"
 import "./index.css"
 import { ThemeProvider } from "styled-components"
 import { theme } from "./theme/index.js"
