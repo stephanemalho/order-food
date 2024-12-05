3f0320d50db0a5063c24617c20f9626e831850c6
Malho Stéphane <stephane.malho@gmail.com>
2024-12-05 13:59:47 +0100

    🏷️ TSE-02 : from string.js to string.ts

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,4 +1,4 @@
-export function convertStringToBoolean(input) {
+export function convertStringToBoolean(input: string) {
   if (typeof input === "boolean") return input
   return input === "true"
 }
