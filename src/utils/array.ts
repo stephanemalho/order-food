1e31eda721b50793295b11ac1e3e4d0de07e6cea
Malho Stéphane <stephane.malho@gmail.com>
2024-12-05 13:59:12 +0100

    🏷️ TSE-02 : from array.js to array.ts

---
+++ src/utils/array.ts
@@ -0,0 +1,40 @@
+export const deepClone = <T>(array: T[]): T[] => {
+  return JSON.parse(JSON.stringify(array));
+};
+
+export const findObjectById = <T extends { id: string | number }>(
+  id: string | number,
+  array: T[]
+): T | undefined => {
+  return array.find((itemInArray) => itemInArray.id === id);
+};
+
+export const findIndexById = <T extends { id: string | number }>(
+  id: string | number,
+  array: T[]
+): number => {
+  return array.findIndex((itemInArray) => itemInArray.id === id);
+};
+
+export const removeObjectById = <T extends { id: string | number }>(
+  idOfItemToRemove: string | number,
+  array: T[]
+): T[] => {
+  return array.filter((item) => item.id !== idOfItemToRemove);
+};
+
+export const isEmpty = <T>(array: T[]): boolean => {
+  return array.length === 0;
+};
+
+
+// const fruits = [{ nom: "Abricot" }, { nom: "Banane" }]
+// const fruitsShallowCopy = [...fruits]
+// const fruitsDeepCopy = deepClone(fruits)
+
+// //fruitsShallowCopy[1].nom = "Cerise"
+// fruitsDeepCopy[1].nom = "Cerise"
+
+// console.log("fruits: ", fruits)
+// //console.log("fruitsShallowCopy: ", fruitsShallowCopy)
+// console.log("fruitsDeepCopy: ", fruitsDeepCopy)
